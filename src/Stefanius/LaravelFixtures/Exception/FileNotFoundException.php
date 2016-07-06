e55fdeda59aba094a228ef7d31897046a43d2b88
Stefanius <sgrootveld@gmail.com>
2016-07-06 23:23:33 +0200

    Removed whitespace.

--- src/Stefanius/LaravelFixtures/Exception/FileNotFoundException.php
+++ src/Stefanius/LaravelFixtures/Exception/FileNotFoundException.php
@@ -10,7 +10,7 @@ class FileNotFoundException extends \Exception
 
     /**
      * FileNotFoundException constructor.
-     * 
+     *
      * @param string    $filename
      * @param Exception $previous
      */
