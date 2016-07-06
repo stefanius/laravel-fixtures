79cbd6cb6608e64d7de00aeba60e81ca37cf768c
Stefanius <sgrootveld@gmail.com>
2016-07-06 23:20:04 +0200

    Add docblocks on exception classes.

--- src/Stefanius/LaravelFixtures/Exception/FileNotFoundException.php
+++ src/Stefanius/LaravelFixtures/Exception/FileNotFoundException.php
@@ -8,6 +8,12 @@ class FileNotFoundException extends \Exception
 {
     protected $message = 'The file "%s" does not exists.';
 
+    /**
+     * FileNotFoundException constructor.
+     * 
+     * @param string    $filename
+     * @param Exception $previous
+     */
     public function __construct($filename, \Exception $previous = null)
     {
         parent::__construct(sprintf($this->message, $filename), 9001, $previous);
